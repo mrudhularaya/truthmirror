629d17ab2cf46048b45e1572f72e2db231298e9c
Mrudhula Raya <51963558+mrudhularaya@users.noreply.github.com>
2025-09-28 13:04:46 +0530

    Create TruthmirrorApplication.groovy

---
+++ src/main/groovy/com/personal/truthmirror/TruthmirrorApplication.groovy
@@ -0,0 +1,13 @@
+package com.personal.truthmirror
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TruthmirrorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TruthmirrorApplication, args)
+	}
+
+}
