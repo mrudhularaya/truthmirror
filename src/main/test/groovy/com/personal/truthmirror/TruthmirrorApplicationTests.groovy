b6adc78db7cf420c2405908d9b3265f6e0b53c6a
Mrudhula Raya <51963558+mrudhularaya@users.noreply.github.com>
2025-09-28 13:55:20 +0530

    Create TruthmirrorApplicationTests.groovy

---
+++ src/main/test/groovy/com/personal/truthmirror/TruthmirrorApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.personal.truthmirror
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TruthmirrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
